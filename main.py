04d4fb2197a710fd6694b50eacddd7c0e5c65f7a
Razi <razi.rezaei@tum.de>
2020-04-13 14:39:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,194 @@
+class Model():
+    def __init__(self):
+        if use_cuda:
+            self.encoder = EncoderRNN().cuda()
+            self.decoder = DecoderRNN().cuda()
+        else:
+            self.encoder = EncoderRNN()
+            self.decoder = DecoderRNN()
+        self.encoder_optimizer = optim.Adam(self.encoder.parameters(), hp.lr)
+        self.decoder_optimizer = optim.Adam(self.decoder.parameters(), hp.lr)
+        self.eta_step = hp.eta_min
+
+    def make_target(self, batch, lengths):
+        if use_cuda:
+            eos = torch.stack([torch.Tensor([0,0,0,0,1])]*batch.size()[1]).cuda().unsqueeze(0)
+        else:
+            eos = torch.stack([torch.Tensor([0,0,0,0,1])]*batch.size()[1]).unsqueeze(0)
+        batch = torch.cat([batch, eos], 0)
+        mask = torch.zeros(Nmax+1, batch.size()[1])
+        for indice,length in enumerate(lengths):
+            mask[:length,indice] = 1
+        if use_cuda:
+            mask = mask.cuda()
+        dx = torch.stack([batch.data[:,:,0]]*hp.M,2)
+        dy = torch.stack([batch.data[:,:,1]]*hp.M,2)
+        p1 = batch.data[:,:,2]
+        p2 = batch.data[:,:,3]
+        p3 = batch.data[:,:,4]
+        p = torch.stack([p1,p2,p3],2)
+        return mask,dx,dy,p
+
+    def train(self, epoch):
+        self.encoder.train()
+        self.decoder.train()
+        batch, lengths = make_batch(hp.batch_size)
+        # encode:
+        z, self.mu, self.sigma = self.encoder(batch, hp.batch_size)
+        # create start of sequence:
+        if use_cuda:
+            sos = torch.stack([torch.Tensor([0,0,1,0,0])]*hp.batch_size).cuda().unsqueeze(0)
+        else:
+            sos = torch.stack([torch.Tensor([0,0,1,0,0])]*hp.batch_size).unsqueeze(0)
+        # had sos at the begining of the batch:
+        batch_init = torch.cat([sos, batch],0)
+        # expend z to be ready to concatenate with inputs:
+        z_stack = torch.stack([z]*(Nmax+1))
+        # inputs is concatenation of z and batch_inputs
+        inputs = torch.cat([batch_init, z_stack],2)
+        # decode:
+        self.pi, self.mu_x, self.mu_y, self.sigma_x, self.sigma_y, \
+            self.rho_xy, self.q, _, _ = self.decoder(inputs, z)
+        # prepare targets:
+        mask,dx,dy,p = self.make_target(batch, lengths)
+        # prepare optimizers:
+        self.encoder_optimizer.zero_grad()
+        self.decoder_optimizer.zero_grad()
+        # update eta for LKL:
+        self.eta_step = 1-(1-hp.eta_min)*hp.R
+        # compute losses:
+        LKL = self.kullback_leibler_loss()
+        LR = self.reconstruction_loss(mask,dx,dy,p,epoch)
+        loss = LR + LKL
+        # gradient step
+        loss.backward()
+        # gradient cliping
+        nn.utils.clip_grad_norm(self.encoder.parameters(), hp.grad_clip)
+        nn.utils.clip_grad_norm(self.decoder.parameters(), hp.grad_clip)
+        # optim step
+        self.encoder_optimizer.step()
+        self.decoder_optimizer.step()
+        # some print and save:
+        if epoch%1==0:
+            print('epoch',epoch,'loss',loss.data[0],'LR',LR.data[0],'LKL',LKL.data[0])
+            self.encoder_optimizer = lr_decay(self.encoder_optimizer)
+            self.decoder_optimizer = lr_decay(self.decoder_optimizer)
+        if epoch%100==0:
+            #self.save(epoch)
+            self.conditional_generation(epoch)
+
+    def bivariate_normal_pdf(self, dx, dy):
+        z_x = ((dx-self.mu_x)/self.sigma_x)**2
+        z_y = ((dy-self.mu_y)/self.sigma_y)**2
+        z_xy = (dx-self.mu_x)*(dy-self.mu_y)/(self.sigma_x*self.sigma_y)
+        z = z_x + z_y -2*self.rho_xy*z_xy
+        exp = torch.exp(-z/(2*(1-self.rho_xy**2)))
+        norm = 2*np.pi*self.sigma_x*self.sigma_y*torch.sqrt(1-self.rho_xy**2)
+        return exp/norm
+
+    def reconstruction_loss(self, mask, dx, dy, p, epoch):
+        pdf = self.bivariate_normal_pdf(dx, dy)
+        LS = -torch.sum(mask*torch.log(1e-5+torch.sum(self.pi * pdf, 2)))\
+            /float(Nmax*hp.batch_size)
+        LP = -torch.sum(p*torch.log(self.q))/float(Nmax*hp.batch_size)
+        return LS+LP
+
+    def kullback_leibler_loss(self):
+        LKL = -0.5*torch.sum(1+self.sigma-self.mu**2-torch.exp(self.sigma))\
+            /float(hp.Nz*hp.batch_size)
+        if use_cuda:
+            KL_min = Variable(torch.Tensor([hp.KL_min]).cuda()).detach()
+        else:
+            KL_min = Variable(torch.Tensor([hp.KL_min])).detach()
+        return hp.wKL*self.eta_step * torch.max(LKL,KL_min)
+
+    def save(self, epoch):
+        sel = np.random.rand()
+        torch.save(self.encoder.state_dict(), \
+            'encoderRNN_sel_%3f_epoch_%d.pth' % (sel,epoch))
+        torch.save(self.decoder.state_dict(), \
+            'decoderRNN_sel_%3f_epoch_%d.pth' % (sel,epoch))
+
+    def load(self, encoder_name, decoder_name):
+        saved_encoder = torch.load(encoder_name)
+        saved_decoder = torch.load(decoder_name)
+        self.encoder.load_state_dict(saved_encoder)
+        self.decoder.load_state_dict(saved_decoder)
+
+    def conditional_generation(self, epoch):
+        batch,lengths = make_batch(1)
+        # should remove dropouts:
+        self.encoder.train(False)
+        self.decoder.train(False)
+        # encode:
+        z, _, _ = self.encoder(batch, 1)
+        if use_cuda:
+            sos = Variable(torch.Tensor([0,0,1,0,0]).view(1,1,-1).cuda())
+        else:
+            sos = Variable(torch.Tensor([0,0,1,0,0]).view(1,1,-1))
+        s = sos
+        seq_x = []
+        seq_y = []
+        seq_z = []
+        hidden_cell = None
+        for i in range(Nmax):
+            input = torch.cat([s,z.unsqueeze(0)],2)
+            # decode:
+            self.pi, self.mu_x, self.mu_y, self.sigma_x, self.sigma_y, \
+                self.rho_xy, self.q, hidden, cell = \
+                    self.decoder(input, z, hidden_cell)
+            hidden_cell = (hidden, cell)
+            # sample from parameters:
+            s, dx, dy, pen_down, eos = self.sample_next_state()
+            #------
+            seq_x.append(dx)
+            seq_y.append(dy)
+            seq_z.append(pen_down)
+            if eos:
+                print(i)
+                break
+        # visualize result:
+        x_sample = np.cumsum(seq_x, 0)
+        y_sample = np.cumsum(seq_y, 0)
+        z_sample = np.array(seq_z)
+        sequence = np.stack([x_sample,y_sample,z_sample]).T
+        make_image(sequence, epoch)
+
+    def sample_next_state(self):
+
+        def adjust_temp(pi_pdf):
+            pi_pdf = np.log(pi_pdf)/hp.temperature
+            pi_pdf -= pi_pdf.max()
+            pi_pdf = np.exp(pi_pdf)
+            pi_pdf /= pi_pdf.sum()
+            return pi_pdf
+
+        # get mixture indice:
+        pi = self.pi.data[0,0,:].cpu().numpy()
+        pi = adjust_temp(pi)
+        pi_idx = np.random.choice(hp.M, p=pi)
+        # get pen state:
+        q = self.q.data[0,0,:].cpu().numpy()
+        q = adjust_temp(q)
+        q_idx = np.random.choice(3, p=q)
+        # get mixture params:
+        mu_x = self.mu_x.data[0,0,pi_idx]
+        mu_y = self.mu_y.data[0,0,pi_idx]
+        sigma_x = self.sigma_x.data[0,0,pi_idx]
+        sigma_y = self.sigma_y.data[0,0,pi_idx]
+        rho_xy = self.rho_xy.data[0,0,pi_idx]
+        x,y = sample_bivariate_normal(mu_x,mu_y,sigma_x,sigma_y,rho_xy,greedy=False)
+        next_state = torch.zeros(5)
+        next_state[0] = x
+        next_state[1] = y
+        next_state[q_idx+2] = 1
+        if use_cuda:
+            return Variable(next_state.cuda()).view(1,1,-1),x,y,q_idx==1,q_idx==2
+        else:
+            return Variable(next_state).view(1,1,-1),x,y,q_idx==1,q_idx==2
+
+
+
 if __name__=="__main__":
     model = Model()
     for epoch in range(70):
